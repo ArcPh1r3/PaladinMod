6c89ba13e61d97f703cb050f6dea16f8329efeaf
TheTimeSweeper <leo.midget@gmail.com>
2026-05-30 19:50:28 -0700

    oops

---
+++ PaladinMod/Modules/DoTs.cs
@@ -0,0 +1,18 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AddressableAssets;
+
+namespace PaladinMod.Modules
+{
+    public class DoTs
+    {
+        public static DotController.DotIndex FuckingCruelSunBurn;
+
+        public static void Init()
+        {
+            FuckingCruelSunBurn = R2API.DotAPI.RegisterDotDef(StaticValues.cruelSunBurnDotInterval, StaticValues.cruelSunBurnDotInterval, DamageColorIndex.Item, Addressables.LoadAsset<BuffDef>("RoR2/Base/Common/bdOnFire.asset").WaitForCompletion());
+        }
+    }
+}
